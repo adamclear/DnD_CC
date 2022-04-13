8e29f7fe2a52f0254dd37ea9a8358294a23042a4
adamclear <3589@holbertonschool.com>
2022-04-13 01:14:39 +0000

    updated

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-from creator import create_app
+from creator import create_app, db
 
 app = create_app()
 app.app_context().push()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
